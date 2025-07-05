151532b1a94c1973b1278d85001f0dd6390b77c9
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2025-07-06 00:17:13 +0100

    who we are

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="p-8">
-    <h1 class="text-xl font-semibold text-pink-700 font-inter">User Dashboard</h1>
+  <div class="min-h-screen text-dark font-inter">
+    <!-- Page Header -->
+    <div
+      class="flex flex-col items-center justify-center gap-8 bg-light-blue-100 mt-12 py-12 px-4 max-w-7xl mx-4 xl:mx-auto rounded-lg page-header"
+    >
+      <img
+        src="../../asset/icons/logo-icon-black.svg"
+        alt=""
+        class="bg-gradient-to-b from-light-blue-500 to-light-blue-300 p-2 rounded-lg w-16 h-16 border-3 border-white"
+      />
+      <h2 class="my-4 text-2xl sm:text-4xl font-semibold font-urbanist">
+        Who we are
+      </h2>
+      <p
+        class="text-xs sm:text-base text-center font-normal px-4 sm:px-0 w-5/6"
+      >
+        MedVax Health is a health technology company on a mission to bridge
+        healthcare gaps through low-cost technology, resilient anti-counterfeit
+        supply chains, and last-mile delivery solutions. We ensure that
+        regardless of geographical barriers, climate disruptions and fragmented
+        healthcare supply chains, life-saving medications, vaccines, and
+        reproductive health products reach the most vulnerable communities, and
+        those who need them most, with a special commitment to bridging the
+        health equity gap for women and girls. We are currently operational in
+        Nigeria, with a vision to expand across Africa.
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.page-header {
+  background-image: url(../../asset/icons/header-abstract.svg);
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: 30rem;
+}
+</style>
